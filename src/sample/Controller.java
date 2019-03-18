423192ffe4f1570a61038cb51773cd8cd19503e8
Kerim Bektasevic <mbektasevi1@etf.unsa.ba>
2019-03-18 11:31:32 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,10 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void SAYHELLOWORLD(ActionEvent actionEvent) {
+
+        System.out.println("Hello world!");
+    }
+}
